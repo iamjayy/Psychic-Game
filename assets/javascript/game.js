d7fd98e75aa9825aa0a9825d60d2c5421f2eb3a5
Jay <jasonnguyen@Jays-MacBook-Pro.local>
2019-03-28 23:39:22 -0400

    psychic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,83 +1,85 @@
-var lettersGuessed = [];
-var guessesLeft = 10;
-var wins = 0;
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+     lettersGuessed = [];
+     guessesLeft = 10;
+     wins = 0;
 
-//use Math.random method along with String.fromCharCode method to generate a random letter
-var computerGuess = String.fromCharCode(Math.round(Math.random() * 26) + 97);
+//use Math.random 
+var computerGuess = computerChoices[Math.round(Math.random() * computerChoices.length)];
 
 console.log(computerGuess);
-//function to capture user's keyboard input and make the input lowercase
 document.onkeydown = function(event) {
-  var keyPress = String.fromCharCode(event.keyCode).toLowerCase();
+    var keyPress = (String.fromCharCode(event.keyCode));
 
-  //document.getElementById('guess').innerHTML = keyPress;
-  addLetter(keyPress);
-};
-
-//function to catch repeat letters and/or add players guess to lettersGuessed string
-function addLetter(usersKeypress) {
-  var repeatGuess = lettersGuessed.some(function(item) {
-    return item === usersKeypress;
-  });
-
-  //alert player if the above code is true.
-  if (repeatGuess) {
-    alert(usersKeypress + " already guessed. Try again!");
-
-    //if it is not a repeat guess, check if it's in word
-  } else {
-    lettersGuessed.push(usersKeypress);
-    console.log(lettersGuessed);
-
-    //show user's input in browser
-    showLettersGuessed();
-    //is user's input a match to computer guess
-    guessMatch(usersKeypress);
-  }
+    addLetter(keyPress);
 }
 
-//function to show letters guessed in browser
+// repeat letters 
+function addLetter (usersKeypress) {
+
+
+    var repeatGuess = lettersGuessed.some(function(item){
+        return item === usersKeypress;
+    })
+
+    //alert player if true.
+    if (repeatGuess) {
+        alert(usersKeypress + " Already Pressed!");
+
+    } else {
+        lettersGuessed.push(usersKeypress);
+        console.log(lettersGuessed);
+
+        //show user's input in browser
+        showLettersGuessed();
+        //is user's input a match to computer guess
+        guessMatch(usersKeypress);
+    }
+
+}
+
+//show letters guessed in browser
 function showLettersGuessed() {
-  var tempStr = lettersGuessed.join(", ");
-  document.getElementById("playersGuess").innerHTML = tempStr;
+    var tempStr = lettersGuessed.join(", ");
+    document.getElementById("playersGuess").innerHTML = tempStr;
 }
 
-function guessMatch(character) {
-  console.log(character);
-  console.log(computerGuess);
+function guessMatch (character) {
 
-  if (character === computerGuess) {
-    alert("You win!");
-    wins = wins + 1;
-    showWins();
-    //toggleGame();
-  } else if (guessesLeft === 0) {
-    alert("Aw man! Lets start over.");
-    resetVariables();
-  } else {
-    guessesLeft = guessesLeft - 1;
-    showGuessesRemaining();
-  }
+    console.log(character);
+    console.log(computerGuess);
+
+    if (character === computerGuess) {
+
+        alert("You win!");
+        wins = wins + 1;
+        showWins();
+
+    } else if (guessesLeft === 0) {
+        alert("start over.");
+        resetVariables ();
+
+    } else {
+        guessesLeft = guessesLeft - 1;
+        showGuessesRemaining();
+    }
 }
 
-//function to show wins
+// show wins
 function showWins() {
-  document.getElementById("numWins").innerHTML = wins;
+    document.getElementById("numWins").innerHTML = wins;
 }
 
-//function to show guesses remaining
+//show loses
+function showWins() {
+    document.getElementById("numLoses").innerHTML = loses;
+}
+
+//show guesses remaining
 function showGuessesRemaining() {
-  document.getElementById("numGuesses").innerHTML = guessesLeft;
+    document.getElementById("numGuesses").innerHTML = guessesLeft;
 }
 
-function resetVariables() {
-  lettersGuessed = [];
-  guessesLeft = 10;
+function resetVariables () {
+    lettersGuessed = [];
+    guessesLeft = 10;
 }
-
-function startGame() {
-  showGuessesRemaining();
-  showWins();
-}
-
-startGame();
